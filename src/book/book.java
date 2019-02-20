b613072b0e64d2e3215a5eaa97ae9c374f38c693
Dinh Son Lam <lam.dinh@pn.com.vn>
2019-02-20 18:17:14 +0700

    khoi tao lan dau tien

---
+++ src/book/book.java
@@ -0,0 +1,13 @@
+
+package book;
+
+
+public interface book {
+    
+    public  void addBook();
+    public void deleteBook();
+    public void updateBook();
+    public  void displayAvailbleBookInfor();
+    public void displayIssueBookInfo();
+    
+}
